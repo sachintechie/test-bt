b044861955a6022b3456f1976f194ee72df7ce75
Ryon <zhangruiyang36@gmail.com>
2024-07-11 00:31:16 -0400

    optimize

--- lib/bridgetower-appsync-stack.ts
+++ lib/bridgetower-appsync-stack.ts
@@ -4,6 +4,16 @@ import {BridgeTowerLambdaStack} from "./bridgetower-lambda-stack";
 import {env, envConfig} from "./env";
 import {configResolver, newAppSyncApi} from "./appsync";
 
+const EXCLUDED_LAMBDAS_IN_APPSYNC = [
+  'apigatewayAuthorizer',
+  'appsyncAuthorizer',
+  'checkAndTransferBonus',
+  'checkTransactionStatusAndUpdate',
+  'deleteKeyAndUserFromCubistAndDB',
+  'mergeStake',
+  'withdrawStake'
+]
+
 export class BridgeTowerAppSyncStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -16,29 +26,12 @@ export class BridgeTowerAppSyncStack extends cdk.Stack {
     // Create a new AppSync GraphQL API
     const api = newAppSyncApi(this, env`Api`, lambdaStack)
 
-    configResolver(api, lambdaStack.getWalletLambda, 'Query', 'GetWallet')
-    configResolver(api, lambdaStack.transferLambda, 'Query', 'Transfer')
-    configResolver(api, lambdaStack.getWalletBalanceLambda, 'Query', 'GetWalletBalance')
-    configResolver(api, lambdaStack.deleteKeyAndUserFromCubistAndDBLambda, 'Query', 'DeleteKeyAndUserFromCubistAndDB')
-    configResolver(api, lambdaStack.checkTransactionStatusAndUpdateLambda, 'Query', 'CheckTransactionStatusAndUpdate')
-    configResolver(api, lambdaStack.checkAndTransferBonusLambda, 'Query', 'CheckAndTransferBonus')
-    configResolver(api, lambdaStack.listWalletTokensLambda, 'Query', 'ListWalletTokens')
-    configResolver(api, lambdaStack.createWalletLambda, 'Query', 'CreateWallet')
-    configResolver(api, lambdaStack.listCustomerWalletsLambda, 'Query', 'ListCustomerWallets')
-    configResolver(api, lambdaStack.listStakeAccountsLambda, 'Query', 'ListStakeAccounts')
-    configResolver(api, lambdaStack.listStakeTransactionsLambda, 'Query', 'ListStakeTransactions')
-    configResolver(api, lambdaStack.signinLambda, 'Query', 'Signin')
-    configResolver(api, lambdaStack.stakingLambda, 'Query', 'Staking')
-    configResolver(api, lambdaStack.unStakingLambda, 'Query', 'UnStaking')
-    configResolver(api, lambdaStack.masterTransferLambda, 'Query', 'MasterTransfer')
-    configResolver(api, lambdaStack.listWalletTransactionsLambda, 'Query', 'ListWalletTransactions')
-    configResolver(api, lambdaStack.mergeStakeLambda, 'Query', 'MergeStake')
-    configResolver(api, lambdaStack.withdrawStakeLambda, 'Query', 'WithdrawStake')
-    configResolver(api, lambdaStack.apigatewayAuthorizerLambda, 'Query', 'ApigatewayAuthorizer')
-    configResolver(api, lambdaStack.getKycAccessTokenLambda, 'Query', 'GetKycAccessToken')
-    configResolver(api, lambdaStack.getKycApplicantLambda, 'Query', 'GetKycApplicant')
-    configResolver(api, lambdaStack.kycWebhookLambda, 'Query', 'KycWebhook')
-
+    // Create resolvers for each lambda function
+    for (const [key, value] of lambdaStack.lambdaMap) {
+      if (!EXCLUDED_LAMBDAS_IN_APPSYNC.includes(key)) {
+        configResolver(api, value, 'Query', capitalize(key))
+      }
+    }
 
     new cdk.CfnOutput(this, env`GraphQLAPIURL`, {
       value: api.graphqlUrl,
@@ -48,4 +41,9 @@ export class BridgeTowerAppSyncStack extends cdk.Stack {
       value: api.apiKey || '',
     });
   }
+}
+
+const capitalize = (s: string) => {
+  if (typeof s !== 'string') return ''
+  return s.charAt(0).toUpperCase() + s.slice(1)
 }
--- lib/bridgetower-lambda-stack.ts
+++ lib/bridgetower-lambda-stack.ts
@@ -1,79 +1,52 @@
-import * as cdk from "aws-cdk-lib";
 import { Stack, StackProps } from "aws-cdk-lib/core";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import {newNodeJsFunction} from "./lambda";
-import {env} from "./env";
+
+// !IMPORTANT:Schema query name should be consistent with resource file name
+const LAMBDA_RESOURCE_NAMES=[
+  "apigatewayAuthorizer",
+  "appsyncAuthorizer",
+  "checkAndTransferBonus",
+  "checkTransactionStatusAndUpdate",
+  "createWallet",
+  "deleteKeyAndUserFromCubistAndDB",
+  "getKycAccessToken",
+  "getKycApplicant",
+  "getWallet",
+  "getWalletBalance",
+  "kycWebhook",
+  "listCustomerWallets",
+  "listStakeAccounts",
+  "listStakeTransactions",
+  "listWalletTokens",
+  "listWalletTransactions",
+  "masterTransfer",
+  "mergeStake",
+  "signin",
+  "staking",
+  "transfer",
+  "unstaking",
+  "withdrawStake",
+]
+
+const APPSYNC_AUTHORIZER_LAMBDA_NAME="appsyncAuthorizer";
 
 
 export class BridgeTowerLambdaStack extends Stack {
-  public readonly getWalletLambda: lambda.Function;
-  public readonly transferLambda: lambda.Function;
-  public readonly appsyncAuthorizerLambda: lambda.Function;
-  public readonly getWalletBalanceLambda: lambda.Function;
-  public readonly deleteKeyAndUserFromCubistAndDBLambda: lambda.Function;
-  public readonly checkTransactionStatusAndUpdateLambda: lambda.Function;
-  public readonly checkAndTransferBonusLambda: lambda.Function;
-  public readonly listWalletTokensLambda: lambda.Function;
-  public readonly createWalletLambda: lambda.Function;
-  public readonly listCustomerWalletsLambda: lambda.Function;
-  public readonly listStakeAccountsLambda: lambda.Function;
-  public readonly listStakeTransactionsLambda: lambda.Function;
-  public readonly signinLambda: lambda.Function;
-  public readonly stakingLambda: lambda.Function;
-  public readonly unStakingLambda: lambda.Function;
-  public readonly masterTransferLambda: lambda.Function;
-  public readonly listWalletTransactionsLambda: lambda.Function;
-  public readonly mergeStakeLambda: lambda.Function;
-  public readonly withdrawStakeLambda: lambda.Function;
-  public readonly apigatewayAuthorizerLambda: lambda.Function;
-  public readonly getKycAccessTokenLambda: lambda.Function;
-  public readonly getKycApplicantLambda: lambda.Function;
-  public readonly kycWebhookLambda: lambda.Function;
+  public readonly lambdaMap: Map<string, lambda.Function>;
 
+  get appsyncAuthorizerLambda(): lambda.Function {
+    return this.lambdaMap.get(APPSYNC_AUTHORIZER_LAMBDA_NAME)!;
+  }
 
   constructor(scope: Construct, id: string, props: StackProps) {
 
     super(scope, id, props);
 
-    this.getWalletLambda=newNodeJsFunction(this, "getWallet", "../resources/getWallet.ts");
-    this.transferLambda=newNodeJsFunction(this, "transfer", "../resources/transfer.ts");
-    this.appsyncAuthorizerLambda=newNodeJsFunction(this, "appsyncAuthorizer", "../resources/appsyncAuthorizer.ts");
-    this.getWalletBalanceLambda=newNodeJsFunction(this, "getWalletBalance", "../resources/getWalletBalance.ts");
-    this.deleteKeyAndUserFromCubistAndDBLambda=newNodeJsFunction(this, "deleteKeyAndUserFromCubistAndDB", "../resources/deleteKeyAndUserFromCubistAndDB.ts");
-    this.checkTransactionStatusAndUpdateLambda=newNodeJsFunction(this, "checkTransactionStatusAndUpdate", "../resources/checkTransactionStatusAndUpdate.ts");
-    this.checkAndTransferBonusLambda=newNodeJsFunction(this, "checkAndTransferBonus", "../resources/checkAndTransferBonus.ts");
-    this.listWalletTokensLambda=newNodeJsFunction(this, "listWalletTokens", "../resources/listWalletTokens.ts");
-    this.createWalletLambda=newNodeJsFunction(this, "createWallet", "../resources/createWallet.ts");
-    this.listCustomerWalletsLambda=newNodeJsFunction(this, "listCustomerWallets", "../resources/listCustomerWallets.ts");
-    this.listStakeAccountsLambda=newNodeJsFunction(this, "listStakeAccounts", "../resources/listStakeAccounts.ts");
-    this.listStakeTransactionsLambda=newNodeJsFunction(this, "listStakeTransactions", "../resources/listStakeTransactions.ts");
-    this.signinLambda=newNodeJsFunction(this, "signin", "../resources/signin.ts");
-    this.stakingLambda=newNodeJsFunction(this, "staking", "../resources/staking.ts");
-    this.unStakingLambda=newNodeJsFunction(this, "unStaking", "../resources/unstaking.ts");
-    this.masterTransferLambda=newNodeJsFunction(this, "masterTransfer", "../resources/masterTransfer.ts");
-    this.listWalletTransactionsLambda=newNodeJsFunction(this, "listWalletTransactions", "../resources/listWalletTransactions.ts");
-    this.mergeStakeLambda=newNodeJsFunction(this, "mergeStake", "../resources/mergeStake.ts");
-    this.withdrawStakeLambda=newNodeJsFunction(this, "withdrawStake", "../resources/withdrawStake.ts");
-    this.apigatewayAuthorizerLambda=newNodeJsFunction(this, "apigatewayAuthorizer", "../resources/apigatewayAuthorizer.ts");
-    this.getKycAccessTokenLambda=newNodeJsFunction(this, "getKycAccessToken", "../resources/getKycAccessToken.ts");
-    this.getKycApplicantLambda=newNodeJsFunction(this, "getKycApplicant", "../resources/getKycApplicant.ts");
-    this.kycWebhookLambda=newNodeJsFunction(this, "kycWebhook", "../resources/kycWebhook.ts");
-
-
-    // Define function URLs for specific Lambdas
-    const getWalletLambdaUrl = this.getWalletLambda.addFunctionUrl({
-      authType: lambda.FunctionUrlAuthType.NONE
-    });
-    const transferLambdaUrl = this.transferLambda.addFunctionUrl({
-      authType: lambda.FunctionUrlAuthType.NONE
-    });
-
-    new cdk.CfnOutput(this, env`GetWalletFunctionUrl`, {
-      value: getWalletLambdaUrl.url
-    });
-    new cdk.CfnOutput(this, env`TransferFunctionUrl`, {
-      value: transferLambdaUrl.url
-    });
+    this.lambdaMap=new Map<string, lambda.Function>();
+    for(const lambdaResourceName of LAMBDA_RESOURCE_NAMES){
+      this.lambdaMap.set(lambdaResourceName, newNodeJsFunction(this, lambdaResourceName, `../resources/${lambdaResourceName}.ts`));
+    }
   }
 }
