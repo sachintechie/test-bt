83cb337c4016eb3fd06e893f3b80d51d24f52241
Ryon <zhangruiyang36@gmail.com>
2024-07-14 01:39:01 -0400

    add prisma

--- resources/lambdas/migrateDB.ts
+++ resources/lambdas/migrateDB.ts
@@ -1,6 +1,15 @@
+import { exec } from 'child_process';
+
 export const handler = async (): Promise<string> => {
   return new Promise((resolve, reject) => {
-    console.log(`Migration successful`);
-    resolve(`Migration successful`);
+    exec('npx prisma migrate deploy', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing migration: ${stderr}`);
+        reject(`Migration failed: ${stderr}`);
+      } else {
+        console.log(`Migration successful: ${stdout}`);
+        resolve(`Migration successful: ${stdout}`);
+      }
+    });
   });
 };
