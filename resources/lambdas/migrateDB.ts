1e39edb145825019cb911c4482ba80096fec0952
Ryon <zhangruiyang36@gmail.com>
2024-07-13 22:35:52 -0400

    add generate

--- resources/lambdas/migrateDB.ts
+++ resources/lambdas/migrateDB.ts
@@ -1,9 +1,24 @@
-import { execSync } from 'child_process';
+import {execSync} from 'child_process';
 import {PrismaClient} from '@prisma/client';
-const prisma = new PrismaClient();
-export const handler = async () => {
+import * as path from "path";
+
+exports.handler = async (event:any) => {
   try {
-    const result = execSync('npx prisma migrate deploy', {
+    // Run prisma generate
+    execSync('npx prisma generate', {
+      cwd: path.resolve(__dirname),
+      env: {
+        ...process.env,
+        DATABASE_URL: process.env.DATABASE_URL,
+      },
+      stdio: 'inherit',
+    });
+
+    // Initialize Prisma client
+    const prisma = new PrismaClient();
+
+    // Your migration logic here
+    execSync('npx prisma migrate deploy', {
       env: {
         ...process.env,
         DATABASE_URL: process.env.DATABASE_URL,
@@ -16,7 +31,6 @@ export const handler = async () => {
       statusCode: 200,
       body: JSON.stringify({ message: 'Migrations applied successfully' }),
     };
-
   } catch (error) {
     console.error('Error applying migrations:', error);
     throw error;
