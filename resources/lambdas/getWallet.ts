63f693de8e18658ec765e5e63096f9bbcb7a5671
Ryon <zhangruiyang36@gmail.com>
2024-07-16 22:14:34 -0400

    create ou and account

--- resources/lambdas/getWallet.ts
+++ resources/lambdas/getWallet.ts
@@ -2,10 +2,10 @@ import * as cs from "@cubist-labs/cubesigner-sdk";
 import { tenant } from "../db/models";
 import { getCsClient, getKey, oidcLogin } from "../cubist/CubeSignerClient";
 import { createCustomer, createWallet, createWalletAndKey, getCustomer, getWalletByCustomer } from "../db/dbFunctions";
-
 const env: any = {
   SignerApiRoot: process.env["CS_API_ROOT"] ?? "https://gamma.signer.cubist.dev"
 };
+
 
 export const handler = async (event: any, context: any) => {
   try {
