e5ec3a1004b0f55edc790306bee48d9b84e5da4f
Sachin Jain <sjain@intervision.com>
2024-08-06 16:01:53 +0530

    send owner user id and delete from cubist org

--- resources/lambdas/deleteKeyAndUserFromCubistAndDB.ts
+++ resources/lambdas/deleteKeyAndUserFromCubistAndDB.ts
@@ -3,7 +3,7 @@ import { getAllCustomerAndWalletByTenant, getTenantCallBackUrl } from "../db/dbF
 
 export const handler = async (event: any) => {
   try {
-    const users = await deleteMasterUserAndWallet();
+    const users = await deleteMasterUserAndWallet(event.arguments.customerWallets);
     return {
       status: 200,
       data: users,
@@ -19,14 +19,13 @@ export const handler = async (event: any) => {
   }
 };
 
-async function deleteMasterUserAndWallet() {
+async function deleteMasterUserAndWallet(customerWallets : string[]) {
   try {
     // const schoolhackTenantId = "46a1ef54-2531-40a0-a42f-308b0598c24a"; //dev
     const schoolhackTenantId = "4a997e70-812f-4c0d-af79-71d0cbb5d562"; //prod
     const tenant = await getTenantCallBackUrl(schoolhackTenantId);
     console.log("Tenant", tenant);
-    // const customerWallets = ["User#cf9fc58e-e2c1-4e5b-a2eb-639c3842fdfb","User#f27ffff3-0a6a-4f88-b305-043f08cdb8e8"]
-    const customerWallets = ["User#2a088449-8006-4cfa-8b71-ca07e5594e74"];
+
     if (tenant != null && customerWallets != null && customerWallets?.length > 0) {
       const updatedCustomer = await deleteMasterCubistUser(customerWallets, tenant.id);
       return {
