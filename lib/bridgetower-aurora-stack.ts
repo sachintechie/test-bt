6504e91ba9fb3e6e8881b8984495300c1c286255
Ryon <zhangruiyang36@gmail.com>
2024-09-03 10:58:01 -0400

    check if existant

--- lib/bridgetower-aurora-stack.ts
+++ lib/bridgetower-aurora-stack.ts
@@ -6,6 +6,8 @@ import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import { env} from "./utils/env";
 import {getVpcConfig} from "./utils/vpc";
 import {getSecurityGroups} from "./utils/security_group";
+import {ISecret} from "aws-cdk-lib/aws-secretsmanager";
+import {IDatabaseCluster} from "aws-cdk-lib/aws-rds";
 
 export const AURORA_CREDENTIALS_SECRET_NAME = 'AuroraCredentials';
 export const DB_NAME = 'auroradb';
@@ -20,38 +22,58 @@ export class AuroraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    let secret: ISecret;
+    try {
+      // Try to retrieve the existing secret by name
+      secret = secretsmanager.Secret.fromSecretNameV2(this, env`${AURORA_CREDENTIALS_SECRET_NAME}`, SECRET_NAME);
+    } catch (error) {
+      if (error.message.includes('does not exist')) {
+        // If the secret does not exist, create a new one
+        const secret = new secretsmanager.Secret(this, env`${AURORA_CREDENTIALS_SECRET_NAME}`, {
+          secretName: SECRET_NAME,
+          generateSecretString: {
+            secretStringTemplate: JSON.stringify({
+              username: USERNAME,
+            }),
+            excludePunctuation: true,
+            includeSpace: false,
+            generateStringKey: 'password',
+            excludeCharacters: '!@#$%^&*()-_+=[]{}|;:,.<>?/`~',
+          },
+        });
+      } else {
+        // Re-throw the error if it's something unexpected
+        throw error;
+      }
+    }
     
-    // Create a secret for the Aurora DB credentials
-    const secret = new secretsmanager.Secret(this, env`${AURORA_CREDENTIALS_SECRET_NAME}`, {
-      secretName: SECRET_NAME,
-      generateSecretString: {
-        secretStringTemplate: JSON.stringify({
-          username: USERNAME,
-        }),
-        excludePunctuation: true,
-        includeSpace: false,
-        generateStringKey: 'password',
-        excludeCharacters: '!@#$%^&*()-_+=[]{}|;:,.<>?/`~',
-      },
-    });
 
-    // Create the Aurora cluster
-    const cluster = new rds.DatabaseCluster(this, env`AuroraCluster`, {
-      engine: rds.DatabaseClusterEngine.auroraPostgres({
-        version: rds.AuroraPostgresEngineVersion.VER_15_4,
-      }),
-      credentials: rds.Credentials.fromSecret(secret),
-      defaultDatabaseName: DB_NAME,
-      instances: 2,
-      instanceProps: {
-        vpc:getVpcConfig(this),
-        instanceType: ec2.InstanceType.of(ec2.InstanceClass.R6I, ec2.InstanceSize.LARGE),
-        securityGroups: getSecurityGroups(this),
-        publiclyAccessible: true, // Make the database publicly accessible
-      },
-      storageEncrypted: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production use
-    });
+    let cluster: IDatabaseCluster;
+    try {
+      // Attempt to retrieve the existing RDS cluster by its identifier
+      cluster = rds.DatabaseCluster.fromDatabaseClusterAttributes(this, env`AuroraCluster`, {
+        clusterIdentifier: env`AuroraCluster`
+      });
+    } catch (error) {
+      // Create the Aurora cluster
+      cluster = new rds.DatabaseCluster(this, env`AuroraCluster`, {
+        clusterIdentifier: env`AuroraCluster`,
+        engine: rds.DatabaseClusterEngine.auroraPostgres({
+          version: rds.AuroraPostgresEngineVersion.VER_15_4,
+        }),
+        credentials: rds.Credentials.fromSecret(secret),
+        defaultDatabaseName: DB_NAME,
+        instances: 2,
+        instanceProps: {
+          vpc:getVpcConfig(this),
+          instanceType: ec2.InstanceType.of(ec2.InstanceClass.R6I, ec2.InstanceSize.LARGE),
+          securityGroups: getSecurityGroups(this),
+          publiclyAccessible: true, // Make the database publicly accessible
+        },
+        storageEncrypted: true,
+        removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production use
+      });
+    }
 
 
     // Output the necessary environment variables
