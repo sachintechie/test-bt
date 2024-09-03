c226ba7e31f1708d15be6fec0bc76b2eee50aa53
Ryon <zhangruiyang36@gmail.com>
2024-09-03 11:22:51 -0400

    ts fix

--- lib/bridgetower-aurora-stack.ts
+++ lib/bridgetower-aurora-stack.ts
@@ -72,7 +72,7 @@ export class AuroraStack extends cdk.Stack {
 
     console.log(secret.secretFullArn)
     this.dbSecretArn = new cdk.CfnOutput(this, env`DBSecretArn`, {
-      value: secret.secretFullArn,
+      value: secret.secretFullArn!,
       description: 'The ARN of the secret storing the DB credentials',
     });
   }
