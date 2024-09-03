0a818c494d9f819ed8ac4b22693d7c707fc6d589
Sachin Jain <sjain@intervision.com>
2024-09-03 14:51:11 +0530

    resolve issue from e-commerce

--- resources/lambdas/batchMintCnft.ts
+++ resources/lambdas/batchMintCnft.ts
@@ -7,61 +7,30 @@ import { getPayerCsSignerKey } from "../cubist/CubeSignerClient";
 // Define your handler function
 export const handler = async (event: any) => {
   try {
-    // Parse the JSON body from the event
-    const body = JSON.parse(event.body || '{}');
-    
+
     // Extract parameters from the parsed body
-    const receiverWalletAddress = body.input?.receiverWalletAddress  as string | undefined;;
-    const amount = body.input?.amount || 1; // Default to 1 if amount is not provided
+
+
+    const receiverWalletAddress = event.arguments?.input?.receiverWalletAddress;
+    const amount = event.arguments?.input?.amount || 1; // Default to 1 if amount is not provided
     const oidcToken = event.headers?.identity;
-    const tenant = event.identity?.resolverContext as tenant
+    const tenant = event.identity?.resolverContext as tenant;
 
     // Validate required fields
     if (!receiverWalletAddress) {
       return {
         statusCode: 400,
-        body: JSON.stringify({ message: "Missing required fields" }),
+        body: JSON.stringify({ message: "Missing required fields" })
       };
     }
-
-    // Create a Solana connection
-    const connection = await getSolConnection();
-
-    // Fetch the payer's keypair (or create one if it doesn't exist)
-    const payer = await getPayerCsSignerKey("Solana", tenant.id);
-    if ( payer?.key == null) {
-      return {
-        transaction: null,
-        error: "Payer Key not found for the given tenant"
-      };
-    }
-
-
-    // Get or create the NFT collection details
-    const collectionDetails = await getOrCreateCollectionNFT(connection, payer);
-
-    // Convert the receiver wallet address to a PublicKey
-    const recipientPublicKey = new PublicKey(receiverWalletAddress);
-
-    // Mint the compressed NFT(s) to the collection
-    const data: MintResult = await mintCompressedNftToCollection(
-      connection,
-      payer.key,
-      collectionDetails.mint,
-      collectionDetails,
-      [recipientPublicKey], // Single recipient; can be expanded to a list
-      amount,
-      oidcToken,
-      tenant
-    );
-
+   const data = await airdropCNFT(tenant, receiverWalletAddress, amount, oidcToken);
     // Build the response
     return {
       statusCode: 200,
       body: JSON.stringify({
         transaction: data.transaction,
-        error: data.error,
-      }),
+        error: data.error
+      })
     };
   } catch (error: any) {
     console.error("Error executing mintCompressedNftToCollection:", error);
@@ -69,8 +38,51 @@ export const handler = async (event: any) => {
       statusCode: 500,
       body: JSON.stringify({
         message: "Internal Server Error",
-        error: error.message,
-      }),
+        error: error.message
+      })
     };
   }
 };
+
+async function airdropCNFT(tenant: tenant, receivers: string[], amount: number, oidcToken: string) {
+  try {
+    // Create a Solana connection
+    const connection = await getSolConnection();
+
+    // Fetch the payer's keypair (or create one if it doesn't exist)
+    const payer = await getPayerCsSignerKey("Solana", tenant.id);
+    if (payer?.key == null) {
+      return {
+        transaction: null,
+        error: "Payer Key not found for the given tenant"
+      };
+    }
+
+    // Get or create the NFT collection details
+    const collectionDetails = await getOrCreateCollectionNFT(connection, payer);
+let receiverList: PublicKey[] = [];
+    // Convert the receiver wallet address to a PublicKey
+    receivers.map((receiver) => {
+      const recipientPublicKey = new PublicKey(receiver);
+      receiverList.push(recipientPublicKey);
+    });
+
+    // Mint the compressed NFT(s) to the collection
+    const data: MintResult = await mintCompressedNftToCollection(
+      connection,
+      payer.key,
+      collectionDetails.mint,
+      collectionDetails,
+      receiverList,
+      amount,
+      oidcToken,
+      tenant
+    );
+
+    return data;
+  } catch (error) {
+    
+      return { data: null, error: error };
+    
+  }
+}
