86ba0e975dc377c58a7155e1489f8886f163e6f6
MudassirIjaz786 <ijazmudassir786@gmail.com>
2024-09-03 17:06:34 +0500

    Filter product api

--- resources/db/models.ts
+++ resources/db/models.ts
@@ -79,7 +79,6 @@ export interface productattribute {
 }
 
 export interface productfilter {
-  //commenting for push
   key: string;
   operator: "gt" | "lt" | "gte" | "lte" | "eq";
   value: number | string;
