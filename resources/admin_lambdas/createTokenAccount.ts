937a81cf4c66584c13655e727ab64e6e713545f7
Sachin Jain <sjain@intervision.com>
2024-09-05 12:55:32 +0530

    add lambda function createTokenAccount

--- resources/admin_lambdas/createTokenAccount.ts
+++ resources/admin_lambdas/createTokenAccount.ts
@@ -54,20 +54,38 @@ export const handler = async (event: any) => {
   }
 };
 
-
-
-async function createTokenAccount(tenant : tenant, senderWalletAddress : string, recipients : any, symbol : string, oidcToken : string, adminUserId : string, chainType : string, tenantTransactionId : string) {
+async function createTokenAccount(
+  tenant: tenant,
+  senderWalletAddress: string,
+  recipients: any,
+  symbol: string,
+  oidcToken: string,
+  adminUserId: string,
+  chainType: string,
+  tenantTransactionId: string
+) {
   try {
-  
     const token = await getTokenBySymbol(symbol);
     console.log("Customer Wallets", recipients, "tenant", tenant, token, "token");
-    const amount = recipients.map((item : any) => Number(item.amount)).reduce((prev : any, curr : any) => prev + curr, 0);
-
-    if (recipients.length > 0 && tenant != null && token != null) {
-      const blockchainTransaction = await createSplTokenAccounts(recipients, token?.decimalprecision ?? 0, chainType, token.contractaddress, oidcToken,senderWalletAddress,tenant);
+    if (token == null) {
+      return {
+        status: 400,
+        data: null,
+        error: "This token not supported at yet"
+      };
+    }
+    if (recipients.length > 0 && tenant != null) {
+      const blockchainTransaction = await createSplTokenAccounts(
+        recipients,
+        token?.decimalprecision ?? 0,
+        chainType,
+        token.contractaddress,
+        oidcToken,
+        senderWalletAddress,
+        tenant
+      );
       if (blockchainTransaction.tokenAccounts != null) {
-     
-        return { tokenAccounts : blockchainTransaction.tokenAccounts, error: null };
+        return { tokenAccounts: blockchainTransaction.tokenAccounts, error: null };
       } else {
         return {
           status: 400,
