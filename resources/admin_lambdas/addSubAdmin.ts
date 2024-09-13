b889389663a379756cfb3d7b432a835ab355ac0c
Sachin Jain <sjain@intervision.com>
2024-09-13 13:12:51 +0530

    Add sub admin lambda function

--- resources/admin_lambdas/addSubAdmin.ts
+++ resources/admin_lambdas/addSubAdmin.ts
@@ -1,12 +1,8 @@
-import * as cs from "@cubist-labs/cubesigner-sdk";
 import { tenant } from "../db/models";
-import { getCsClient } from "../cubist/CubeSignerClient";
-import { createAdminUser, getAdminUser } from "../db/adminDbFunctions";
+import { createAdminUser, getAdminUser, getAdminUserByEmail } from "../db/adminDbFunctions";
 import AWS from "aws-sdk";
 const cognito = new AWS.CognitoIdentityServiceProvider();
-const env: any = {
-  SignerApiRoot: process.env["CS_API_ROOT"] ?? "https://gamma.signer.cubist.dev"
-};
+
 const ADMIN_GROUP = process.env["ADMIN_GROUP"];
 
 export const handler = async (event: any, context: any) => {
@@ -15,10 +11,9 @@ export const handler = async (event: any, context: any) => {
 
     const data = await createUser(
       event.identity.resolverContext as tenant,
-      event.arguments?.input?.tenantUserId,
+      event.arguments?.input?.emailId,
       event.arguments?.input?.name,
-      event.arguments?.input?.password,
-      event.headers?.identity
+      event.arguments?.input?.password
     );
 
     const response = {
@@ -39,125 +34,86 @@ export const handler = async (event: any, context: any) => {
   }
 };
 
-async function createUser(tenant: tenant, tenantuserid: string, username: string, password: string, oidcToken: string) {
+async function createUser(tenant: tenant, emailId: string, username: string, password: string) {
   console.log("Creating admin user");
 
   try {
-    console.log("createUser", tenant.id, tenantuserid);
-    const customer = await getAdminUser(tenantuserid, tenant.id);
+    console.log("createUser", tenant.id, emailId);
+    const customer = await getAdminUserByEmail(emailId, tenant.id);
     if (customer != null && customer?.cubistuserid) {
       return { customer, error: null };
     } else {
-      if (!oidcToken) {
+      try {
+        if (tenant.iscognitoactive && tenant.userpoolid) {
+          // Define parameters for creating a new Cognito user
+          const params: AWS.CognitoIdentityServiceProvider.AdminCreateUserRequest = {
+            UserPoolId: tenant.userpoolid, // Replace with your Cognito User Pool ID
+            Username: emailId ? emailId : "", // Username of the user to be created
+
+            UserAttributes: [
+              {
+                Name: "email",
+                Value: emailId ? emailId : "" // Optional: user's email address
+              }
+            ],
+            MessageAction: "SUPPRESS", // Prevents the automatic email invitation
+            TemporaryPassword: password // Set a temporary password
+          };
+
+          // Create the user in Cognito
+          const createUserResponse = await cognito.adminCreateUser(params).promise();
+          console.log("Created Cognito user", createUserResponse);
+          // Optionally, set a permanent password for the user
+          const setPasswordParams: AWS.CognitoIdentityServiceProvider.AdminSetUserPasswordRequest = {
+            UserPoolId: tenant.userpoolid,
+            Username: emailId ? emailId : "",
+            Password: password,
+            Permanent: true
+          };
+
+          await cognito.adminSetUserPassword(setPasswordParams).promise();
+          // Define parameters for adding a user to the group
+          const groupParams: AWS.CognitoIdentityServiceProvider.AdminAddUserToGroupRequest = {
+            UserPoolId: tenant.userpoolid, // Replace with your Cognito User Pool ID
+            Username: emailId ? emailId : "", // Username of the user to be added to the group
+            GroupName: ADMIN_GROUP ? ADMIN_GROUP : "Admin" // Name of the group to add the user to
+          };
+          await cognito.adminAddUserToGroup(groupParams).promise();
+        }
+        const customer = await createAdminUser({
+          emailid: emailId ? emailId : "",
+          name: username ? username : username,
+          tenantuserid: emailId ? emailId : "",
+          tenantid: tenant.id,
+          isactive: true,
+          isBonusCredit: false,
+          createdat: new Date().toISOString()
+        });
+        console.log("Created customer", customer.id);
+
+        const customerData = {
+          cubistuserid: null,
+          tenantuserid: emailId ? emailId : "",
+          tenantid: tenant.id,
+          emailid: emailId,
+          id: customer.id,
+          createdat: new Date().toISOString()
+        };
+
+        return { customer: customerData, error: null };
+      } catch (e) {
+        console.log(`Not verified: ${e}`);
         return {
           customer: null,
-          error: "Please provide an identity token for verification"
+          error: e
         };
-      } else {
-        try {
-          const { client, org, orgId } = await getCsClient(tenant.id);
-          if (client == null || org == null) {
-            return {
-              customer: null,
-              error: "Error creating cubesigner client"
-            };
-          }
-          console.log("Created cubesigner client", client);
-          const proof = await cs.CubeSignerClient.proveOidcIdentity(env, orgId, oidcToken);
-
-          console.log("Verifying identity", proof);
-
-          await org.verifyIdentity(proof);
-
-          console.log("Verified");
-
-          //assert(proof.identity, "Identity should be set when proof is obtained using OIDC token");
-          const iss = proof.identity!.iss;
-          const sub = proof.identity!.sub;
-          const email = proof.email;
-          const name = proof.preferred_username;
-          let cubistUserId;
-          // If user does not exist, create it
-          if (!proof.user_info?.user_id) {
-            console.log(`Creating OIDC user ${email}`);
-            cubistUserId = await org.createOidcUser({ iss, sub }, email, {
-              name,
-              memberRole: "Member"
-            });
-          } else {
-            cubistUserId = proof.user_info?.user_id;
-          }
-          
-          if(tenant.iscognitoactive && tenant.userpoolid){
-            // Define parameters for creating a new Cognito user
-            const params: AWS.CognitoIdentityServiceProvider.AdminCreateUserRequest = {
-              UserPoolId: tenant.userpoolid, // Replace with your Cognito User Pool ID
-              Username:  email ? email : "" , // Username of the user to be created
-
-              UserAttributes: [
-                {
-                  Name: "email",
-                  Value: email ? email : "" // Optional: user's email address
-                }
-              ],
-              MessageAction: "SUPPRESS", // Prevents the automatic email invitation
-              TemporaryPassword: password // Set a temporary password
-            };
-  
-            // Create the user in Cognito
-            const createUserResponse = await cognito.adminCreateUser(params).promise();
-            console.log("Created Cognito user", createUserResponse);
-            // Optionally, set a permanent password for the user
-            const setPasswordParams: AWS.CognitoIdentityServiceProvider.AdminSetUserPasswordRequest = {
-              UserPoolId: tenant.userpoolid,
-              Username: email ? email : "",
-              Password: password,
-              Permanent: true
-            };
-  
-            await cognito.adminSetUserPassword(setPasswordParams).promise();
-            // Define parameters for adding a user to the group
-            const groupParams: AWS.CognitoIdentityServiceProvider.AdminAddUserToGroupRequest = {
-              UserPoolId: tenant.userpoolid, // Replace with your Cognito User Pool ID
-              Username: email ? email : "", // Username of the user to be added to the group
-              GroupName: ADMIN_GROUP ? ADMIN_GROUP : "Admin" // Name of the group to add the user to
-            };
-            await cognito.adminAddUserToGroup(groupParams).promise();
-          }
-          const customer = await createAdminUser({
-            emailid: email ? email : "",
-            name: name ? name : username,
-            tenantuserid,
-            tenantid: tenant.id,
-            iss: iss,
-            cubistuserid: cubistUserId,
-            isactive: true,
-            isBonusCredit: false,
-            createdat: new Date().toISOString()
-          });
-          console.log("Created customer", customer.id);
-        
-          const customerData = {
-            cubistuserid: cubistUserId,
-            tenantuserid: tenantuserid,
-            tenantid: tenant.id,
-            emailid: email,
-            id: customer.id,
-            createdat: new Date().toISOString()
-          };
-
-          return { customer: customerData, error: null };
-        } catch (e) {
-          console.log(`Not verified: ${e}`);
-          return {
-            customer: null,
-            error: "Please send a valid identity token for verification"
-          };
-        }
       }
     }
   } catch (e) {
-    console.log(e);
-    throw e;
+    console.log(`Not verified: ${e}`);
+    return {
+      customer: null,
+      error: e
+    };
   }
 }
