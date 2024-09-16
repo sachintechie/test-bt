15ee17664bf0409206e978420c81ac103353be55
Ryon <zhangruiyang36@gmail.com>
2024-09-16 19:49:08 -0400

    NFT transfer

--- resources/lambdas/transferNFT.ts
+++ resources/lambdas/transferNFT.ts
@@ -1,10 +1,10 @@
 import Web3 from "web3";
 import contractAbi from '../abi/BridgeTowerNftUpgradeable.json';
-
+import {getPayerCsSignerKey} from "../cubist/CubeSignerClient";
+import {tenant} from "../db/models";
 
 const AVAX_RPC_URL = process.env.AVAX_RPC_URL!;
 const ETH_RPC_URL = process.env.ETH_RPC_URL!;
-const PRIVATE_KEY = process.env.PRIVATE_KEY!;
 const CONTRACT_ABI = contractAbi.abi;
 
 const web3Avax = new Web3(AVAX_RPC_URL);
@@ -12,22 +12,11 @@ const web3Eth = new Web3(ETH_RPC_URL);
 
 export const handler = async (event: any, context: any) => {
   const {  toAddress, tokenIds, chain, contractAddress} = event.arguments?.input;
-  const web3=chain==='AVAX'?web3Avax:web3Eth;
-
-  const account = web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY);
-  web3.eth.accounts.wallet.add(account);
-
-  const contract = new web3.eth.Contract(CONTRACT_ABI, contractAddress);
-
-  const tx = {
-    from: account.address,
-    to: contractAddress,
-    gas: 300000,
-    data:  contract.methods.batchTransfer(account.address, toAddress, tokenIds).encodeABI()
-  };
 
   try {
-    const receipt = await web3.eth.sendTransaction(tx);
+    const tenant= event.identity.resolverContext as tenant;
+    const tenantId=tenant.id;
+    const receipt =await transferNFT(toAddress, tokenIds, chain, contractAddress,tenantId);
     return {
       status: 200,
       transactionHash: receipt.transactionHash,
@@ -41,3 +30,29 @@ export const handler = async (event: any, context: any) => {
     };
   }
 };
+
+export const transferNFT = async (toAddress: string, tokenIds: any, chain: string, contractAddress: string,tenantId:string) => {
+  const web3=chain==='AVAX'?web3Avax:web3Eth;
+  const payerKey = await getPayerCsSignerKey("Ethereum", tenantId);
+
+  const contract = new web3.eth.Contract(CONTRACT_ABI, contractAddress);
+  const currentNonce = await web3.eth.getTransactionCount(payerKey.key?.materialId!, 'pending');
+  const tx:any = {
+    from: payerKey.key?.materialId,
+    to: contractAddress,
+    type:'0x02',
+    maxPriorityFeePerGas: web3.utils.toWei('1', 'gwei'), // Priority fee for miners
+    maxFeePerGas: web3.utils.toWei('30', 'gwei'),        // Maximum fee you're willing to pay
+    data: contract.methods.batchTransfer(payerKey.key?.materialId, toAddress, tokenIds).encodeABI(),
+    nonce: `0x${  currentNonce.toString(16)}`
+  };
+
+  // Estimate gas for the transaction if needed
+  const gasEstimate = await web3.eth.estimateGas(tx);
+  tx.gas  = `0x${  gasEstimate.toString(16)}`;
+  // Adjust the gas limit accordingly if required
+  console.log(tx)
+
+  const signedTx = await payerKey.key?.signEvm({tx,chain_id:43113});
+  return web3.eth.sendSignedTransaction(signedTx?.data()?.rlp_signed_tx || '');
+}
