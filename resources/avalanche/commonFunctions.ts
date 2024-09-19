2b382659cd62ec972914921544b030eb8c71c2f5
Sachin Jain <sjain@intervision.com>
2024-09-19 15:37:27 +0530

    changes in avax common functions

--- resources/avalanche/commonFunctions.ts
+++ resources/avalanche/commonFunctions.ts
@@ -1,54 +1,46 @@
-
-
-import { avm, pvm, evm } from '@avalabs/avalanchejs';
+import { avm, pvm, evm } from "@avalabs/avalanchejs";
 
 export async function getAvaxBalance(address: string) {
-  try{
- 
-    const pAddress: string = "P-" + address; 
+  try {
+    const pAddress: string = "P-" + address;
     const { avmapi, pvmapi } = await getAvaxConnection();
-    console.log(`Fetching balance for address: ${await pvmapi.getBalance({ addresses: [pAddress] })}`);
+    const balanceRes = await pvmapi.getBalance({ addresses: [pAddress] });
+    console.log(balanceRes);
+    //let sendingAmount = parseFloat(amount.toString());
+    let LAMPORTS_PER_AVAX = 10 ** 9;
+    console.log("LAMPORTS_PER_SPLTOKEN", LAMPORTS_PER_AVAX);
 
-
-    const { utxos } = await pvmapi.getUTXOs({ addresses: [address] });
-
-console.log("P-Chain Balance:" + await pvmapi.getUTXOs({ addresses: [pAddress] }));
-
-    const balance = utxos.toString();
-    console.log(`P-Chain Balance: ${balance} nAVAX`);
-  
-    return Number.parseInt(balance);}
-    catch (error) {
-      console.error("Error fetching balance:", error);
-      return 0;
-    }
+    const balance = Number(balanceRes.balance) / LAMPORTS_PER_AVAX;
+    return balance;
+  } catch (error) {
+    console.error("Error fetching balance:", error);
+    return 0;
   }
-  
-  export async function getAvaxConnection() {
-    const pvmapi = new pvm.PVMApi("https://api.avax-test.network/ext/bc/P");
-    const avmapi = new avm.AVMApi("https://api.avax-test.network/ext/bc/P");
-
-    // const ip: string = process.env["AVAX_URL"]! // Testnet URL
-    // const port: number = 443; 
-    // const protocol: string = "https";
-     const AVAX_NETWORK_ID: string = process.env["AVAX_NETWORK_ID"]!; 
-     const networkID: number = Number.parseInt(AVAX_NETWORK_ID); 
-  //  const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID);
-    // const xchain: AVMAPI = avalanche.XChain();
-    // const pchain: PlatformVMAPI = avalanche.PChain(); 
-        return { pvmapi, avmapi, networkID };
 }
 
+export async function getAvaxConnection() {
+  const pvmapi = new pvm.PVMApi("https://api.avax-test.network");
+  const avmapi = new avm.AVMApi("https://api.avax-test.network");
 
+  // const ip: string = process.env["AVAX_URL"]! // Testnet URL
+  // const port: number = 443;
+  // const protocol: string = "https";
+  const AVAX_NETWORK_ID: string = process.env["AVAX_NETWORK_ID"]!;
+  const networkID: number = Number.parseInt(AVAX_NETWORK_ID);
+  //  const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID);
+  // const xchain: AVMAPI = avalanche.XChain();
+  // const pchain: PlatformVMAPI = avalanche.PChain();
+  return { pvmapi, avmapi, networkID };
+}
 
-  export async function verifyAvalancheTransaction(txID: string) {
-    try {
-        const { pvmapi } = await getAvaxConnection();
-      const status = await pvmapi.getTxStatus({txID});
-      console.log(`Transaction Status: ${status.status}`);
-      return status;
-    } catch (error) {
-      console.error("Error fetching transaction status:", error);
-      return null;
-    }
+export async function verifyAvalancheTransaction(txID: string) {
+  try {
+    const { pvmapi } = await getAvaxConnection();
+    const status = await pvmapi.getTxStatus({ txID });
+    console.log(`Transaction Status: ${status.status}`);
+    return status;
+  } catch (error) {
+    console.error("Error fetching transaction status:", error);
+    return null;
   }
+}
