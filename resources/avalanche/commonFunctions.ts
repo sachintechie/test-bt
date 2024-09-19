51405005f39c2116d87d4171569a4c7f122a0fc3
Sachin Jain <sjain@intervision.com>
2024-09-19 13:18:59 +0530

    changes in getAVAX balance

--- resources/avalanche/commonFunctions.ts
+++ resources/avalanche/commonFunctions.ts
@@ -1,38 +1,39 @@
-import { Avalanche, BinTools, Buffer } from "avalanche";
 import { AVMAPI } from "avalanche/dist/apis/avm";
 import { PlatformVMAPI } from "avalanche/dist/apis/platformvm";
 
-
-
+import { Avalanche, Buffer, BinTools } from 'avalanche';
 export async function getAvaxBalance(address: string) {
   try{
     const pAddress: string = "P-" + address; 
     console.log(`Fetching balance for address: ${pAddress}`);
     const { xchain, pchain } = await getAvaxConnection();
   
+console.log("P-Chain Balance:" + await pchain.getBalance(address));
+console.log("P-Chain Balance:" + await pchain.getBalance(pAddress));
 
     const balanceResponse = await pchain.getTx(pAddress);
-    const balance = balanceResponse;
+    const balance = balanceResponse.toString();
     console.log(`P-Chain Balance: ${balance} nAVAX`);
     const balanceres = await pchain.getTx(address);
     console.log("P-Chain Balance:" + balanceres);
   
-    return balance;}
+    return Number.parseInt(balance);}
     catch (error) {
       return 0;
     }
   }
   
   export async function getAvaxConnection() {
-    const ip: string = process.env["AVAX_URL"]! // Testnet URL
-    const port: number = 443; 
-    const protocol: string = "https";
-    const AVAX_NETWORK_ID: string = process.env["AVAX_NETWORK_ID"]!; 
-    const networkID: number = Number.parseInt(AVAX_NETWORK_ID); 
-    const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID);
+    const avalanche = new Avalanche('testnet'); // Connect to Avalanche testnet
+    // const ip: string = process.env["AVAX_URL"]! // Testnet URL
+    // const port: number = 443; 
+    // const protocol: string = "https";
+     const AVAX_NETWORK_ID: string = process.env["AVAX_NETWORK_ID"]!; 
+     const networkID: number = Number.parseInt(AVAX_NETWORK_ID); 
+  //  const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID);
     const xchain: AVMAPI = avalanche.XChain();
     const pchain: PlatformVMAPI = avalanche.PChain(); 
-    return { xchain, pchain, networkID };
+        return { xchain, pchain, networkID };
 }
 
 
