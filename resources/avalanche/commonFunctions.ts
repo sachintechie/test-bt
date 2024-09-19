18b3099ea51cbec17fe7ed8c84f631f64ec8fe1d
Sachin Jain <sjain@intervision.com>
2024-09-19 10:47:06 +0530

    add avax url and id to the env

--- resources/avalanche/commonFunctions.ts
+++ resources/avalanche/commonFunctions.ts
@@ -6,25 +6,23 @@ import { PlatformVMAPI } from "avalanche/dist/apis/platformvm";
 
 export async function getAvaxBalance(address: string) {
     const pAddress: string = "P-" + address; 
-
     const { xchain, pchain } = await getAvaxConnection();
     const balanceResponse = await pchain.getBalance(pAddress);
     const balance = balanceResponse.balance;
     console.log(`P-Chain Balance: ${balance} nAVAX`);
     return balance;
   }
-
+  
   export async function getAvaxConnection() {
-    const ip: string = "api.avax.network";
-    const port: number = 443;
+    const ip: string = process.env["AVAX_URL"]! // Testnet URL
+    const port: number = 443; 
     const protocol: string = "https";
-    const networkID: number = 1; // Mainnet ID is 1
-    //const networkID = Defaults.network['mainnet'].networkID;
+    const networkID: number = Number(process.env["AVAX_NETWORK_ID"]); // Avalanche Testnet ID
     const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID);
     const xchain: AVMAPI = avalanche.XChain();
-    const pchain: PlatformVMAPI = avalanche.PChain();
+    const pchain: PlatformVMAPI = avalanche.PChain(); 
     return { xchain, pchain, networkID };
-  }
+}
 
   export async function verifyAvalancheTransaction(txID: string) {
     try {
