7a32a4213ad0670dfee70748aa2dc10b2560fd40
Sachin Jain <sjain@intervision.com>
2024-10-07 10:59:26 +0530

    uncomment both stack

--- bin/bridgetower.ts
+++ bin/bridgetower.ts
@@ -19,17 +19,17 @@ if (!isDevOrProd() && !isOnDemandProd() && !isPlaygroundDev()) {
 }
 
 // Create the stack with an environment-specific ID
-// new BridgeTowerAppSyncStack(app, env`BTAppSyncStack`, {
-//   env: envConfig,
-//   lambdaFolder: "../../resources/lambdas",
-//   schemaFile: "schema.graphql",
-//   name: "GraphQLAPI",
-//   authorizerLambda: "appsyncAuthorizer",
-//   hasApiGateway: true,
-//   apiName: "Api",
-//   needMigrate: true,
-//   auroraStack: auroraStack
-// });
+new BridgeTowerAppSyncStack(app, env`BTAppSyncStack`, {
+  env: envConfig,
+  lambdaFolder: "../../resources/lambdas",
+  schemaFile: "schema.graphql",
+  name: "GraphQLAPI",
+  authorizerLambda: "appsyncAuthorizer",
+  hasApiGateway: true,
+  apiName: "Api",
+  needMigrate: true,
+  auroraStack: auroraStack
+});
 
 new BridgeTowerAppSyncStack(app, env`BTAppSyncStackAdmin`, {
   env: envConfig,
