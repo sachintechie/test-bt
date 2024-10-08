b655bedec280cf97f804a301f9767e51c2f0e0a7
MudassirIjaz786 <ijazmudassir786@gmail.com>
2024-10-08 23:11:47 +0400

    review identity added

--- resources/lambdas/getReviewsByCustomerId.ts
+++ resources/lambdas/getReviewsByCustomerId.ts
@@ -1,10 +1,17 @@
 import { getReviewsByCustomerId } from "../db/dbFunctions";
-
+import { tenant } from "../db/models";
 export const handler = async (event: any) => {
   try {
-    console.log(event);
+    const tenant = event.identity.resolverContext as tenant;
+    const customerId = tenant?.customerid;
 
-    const customerId = event.arguments?.input?.customerId;
+    if (!customerId) {
+      return {
+        status: 400,
+        data: null,
+        error: "Customer ID must be provided."
+      };
+    }
     const review = await getReviewsByCustomerId(customerId);
     return {
       status: 200,
