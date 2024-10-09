1e72e06849bf847842a60733e960b96c316fc643
Sachin Jain <sjain@intervision.com>
2024-10-09 12:44:38 +0530

    store hash on avax subnet

---
+++ resources/lambdas/storeHashOnSubnet.ts
@@ -0,0 +1,31 @@
+import {  storeHashOnSubnet } from "../avalanche/storeHashFunctions";
+
+export const handler = async (event: any) => {
+  try {
+    console.log(event);
+
+    if (event.arguments?.input?.chainType === "Avalanche") {
+      const hash = await storeHashOnSubnet(event.arguments?.input?.hash);
+
+      const response = {
+        status: hash?.data != null ? 200 : 400,
+        data: hash?.data,
+        error: hash?.error
+      };
+      return response;
+    } else {
+      return {
+        status: 400,
+        data: null,
+        error: "ChainType not supported"
+      };
+    }
+  } catch (err) {
+    console.log("In catch Block Error", err);
+    return {
+      status: 400,
+      data: null,
+      error: err
+    };
+  }
+};
