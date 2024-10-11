45dbbcb3811f5c93391168d71956aa0d1b4ef1c4
Ryon <zhangruiyang36@gmail.com>
2024-10-11 12:57:15 -0400

    new source

--- lib/utils/appsync.ts
+++ lib/utils/appsync.ts
@@ -40,7 +40,7 @@ export const newAppSyncApi = (
 };
 
 export const configResolver = (api: GraphqlApi, lambda: IFunction, typeName: string, fieldName: string) => {
-  const dataSource = api.addLambdaDataSource(env`${fieldName}LambdaDataSource`, lambda);
+  const dataSource = api.addLambdaDataSource(env`${fieldName}LambdaFuncDataSource`, lambda);
   dataSource.createResolver(env`${fieldName}Resolver`, {
     typeName: typeName,
     fieldName: fieldName,
