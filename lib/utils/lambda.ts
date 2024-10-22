cc257a444e4440b5846aee02312bb58cd70bdb94
Ryon <zhangruiyang36@gmail.com>
2024-10-21 21:49:33 -0400

    new migration

--- lib/utils/lambda.ts
+++ lib/utils/lambda.ts
@@ -60,3 +60,38 @@ export const newNodeJsFunction = (
     },
   });
 };
+
+export const newMigrateNodeJsFunction = (scope: Construct, id: string, resourcePath: string, dataInfo: DatabaseInfo, memorySize: number = 512) => {
+  return new NodejsFunction(scope, env`${id}`, {
+    functionName: env`${id}-function`,
+    description: getDescription(),
+    runtime: lambda.Runtime.NODEJS_18_X,
+    entry: path.join(__dirname, resourcePath),
+    timeout: cdk.Duration.minutes(15),
+    memorySize: memorySize,
+    environment: { ...getEnvConfig(dataInfo) },
+    vpc: getVpcConfig(scope),
+    securityGroups: getSecurityGroups(scope),
+    role: getLambdaRole(scope),
+    bundling: {
+      nodeModules: ["@prisma/client", "prisma"],
+      minify: true,
+      commandHooks: {
+        beforeBundling(inputDir: string, outputDir: string): string[] {
+          return [];
+        },
+        beforeInstall(inputDir: string, outputDir: string): string[] {
+          return [`cp -R ${inputDir}/prisma ${outputDir}/`];
+        },
+        afterBundling(inputDir: string, outputDir: string): string[] {
+          return [
+            `npx prisma generate --schema=${outputDir}/prisma/schema.prisma`,
+            `cp ${inputDir}/package.json ${outputDir}/node_modules/`,
+            `cp ${inputDir}/package-lock.json ${outputDir}/node_modules/`,
+            `cp -R ${outputDir}/node_modules/prisma/build/* ${outputDir}/node_modules/.bin/`
+          ];
+        }
+      }
+    }
+  });
+};
