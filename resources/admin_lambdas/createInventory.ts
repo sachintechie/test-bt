b37cdee8fb7c72a123ce22d1f068d900c4a71695
MudassirIjaz786 <ijazmudassir786@gmail.com>
2024-11-01 17:09:33 +0400

    get seller ownership added for transfer

--- resources/admin_lambdas/createInventory.ts
+++ resources/admin_lambdas/createInventory.ts
@@ -1,14 +1,16 @@
 import { tenant } from "../db/models";
-import { createInventory } from "../db/adminDbFunctions"; 
+import { createInventory, addOwnership, getAdminUserById } from "../db/adminDbFunctions";
+import { getCustomer } from "../db/dbFunctions";
 
 export const handler = async (event: any, context: any) => {
   try {
     console.log("event", event, "context", context);
 
-    const { productId, inventoryId, inventoryCategory, price, quantity, ownershipNft, smartContractAddress, tokenId,  } = event.arguments?.input;
-    const tenantContext = event.identity.resolverContext as tenant;
-  
-    if (!productId || !inventoryCategory || !price || !quantity  || !inventoryId ) {
+    const { productId, inventoryId, inventoryCategory, price, quantity, ownershipNft, smartContractAddress, tokenId } =
+      event.arguments?.input;
+    const tenant = event.identity.resolverContext as tenant;
+
+    if (!productId || !inventoryCategory || !price || !quantity || !inventoryId) {
       return {
         status: 400,
         data: null,
@@ -16,16 +18,26 @@ export const handler = async (event: any, context: any) => {
       };
     }
 
-    const inventory = await createInventoryInDb( {
-  	  inventoryId,
+    const inventory = await createInventoryInDb({
+      inventoryId,
       productId,
       inventoryCategory,
       price,
       quantity,
       ownershipNft,
       smartContractAddress,
-      tokenId,
+      tokenId
     });
+
+    if (inventory) {
+      const adminUser = await getAdminUserById(tenant.adminuserid!);
+      console.log("adminUser", adminUser);
+      const customer = await getCustomer(adminUser?.tenantuserid!, tenant.id!);
+      console.log("customer", customer);
+      if (customer) {
+        await addOwnership(inventory.id, customer.id!);
+      }
+    }
 
     return {
       status: 200,
@@ -46,16 +58,16 @@ export const handler = async (event: any, context: any) => {
   }
 };
 
-async function createInventoryInDb( inventoryData: any) {
+async function createInventoryInDb(inventoryData: any) {
   const newInventory = await createInventory({
-  	inventoryid: inventoryData.inventoryId,
+    inventoryid: inventoryData.inventoryId,
     productid: inventoryData.productId,
     inventorycategory: inventoryData.inventoryCategory,
     price: inventoryData.price,
     quantity: inventoryData.quantity,
     ownershipnft: inventoryData.ownershipNft,
     smartcontractaddress: inventoryData.smartContractAddress,
-    tokenid: inventoryData.tokenId,	
+    tokenid: inventoryData.tokenId
   });
   return newInventory;
 }
---
+++ resources/lambdas/getOwnershipByInventoryId.ts
@@ -0,0 +1,40 @@
+import { getOwnershipByInventoryId } from "../db/dbFunctions";
+
+export const handler = async (event: any, context: any) => {
+  try {
+    console.log(event, context);
+
+    const { inventoryId } = event.arguments?.input;
+
+    if (!inventoryId) {
+      return {
+        status: 400,
+        data: null,
+        error: "Invalid input: inventoryId is required"
+      };
+    }
+
+    const ownership = await getOwnershipByInventoryId(inventoryId);
+
+    if (!inventoryId) {
+      return {
+        status: 404,
+        data: null,
+        error: "No inventory found for the given inventoryId"
+      };
+    }
+
+    return {
+      status: 200,
+      data: ownership,
+      error: null
+    };
+  } catch (error) {
+    console.error("Error retrieving ownership:", error);
+    return {
+      status: 500,
+      data: null,
+      error: "Internal Server Error"
+    };
+  }
+};
