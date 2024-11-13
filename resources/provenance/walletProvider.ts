1a40d66c0d4ea9c93a992a69df2d7af904bc51a8
Aditya Dhir <adhir@intervision.com>
2024-11-13 22:10:37 +0530

    fix

--- resources/provenance/walletProvider.ts
+++ resources/provenance/walletProvider.ts
@@ -21,6 +21,7 @@ export class CosmosSecp256k1CubeSigner implements OfflineDirectSigner {
 
   constructor(key: cs.Key) {
     this.account = CosmosSecp256k1CubeSigner.keyToAccountData(key);
+    this.key = key;
   }
 
   /** inheritdoc */
