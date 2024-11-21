4c2cb61cfe75d9ecf5f5678598e865f53ce0fba3
sachintechie <115137978+sachintechie@users.noreply.github.com>
2024-11-21 17:05:05 +0530

    Update lambda.ts

--- lib/utils/lambda.ts
+++ lib/utils/lambda.ts
@@ -40,11 +40,7 @@ export const newNodeJsFunction = (scope: Construct, id: string, resourcePath: st
       externalModules: [],
       commandHooks: {
         beforeBundling(inputDir: string, outputDir: string): string[] {
-          return [
-                        `rm -rf ${outputDir}/node_modules/pdf-parse/test`, // Remove unnecessary pdf-parse/test folder
-
-]; 
-// No additional commands before bundling
+          return []; // No additional commands before bundling
         },
         beforeInstall(inputDir: string, outputDir: string): string[] {
           return [`cp -R ${inputDir}/prisma ${outputDir}/`]; // Copy Prisma schema before installation
