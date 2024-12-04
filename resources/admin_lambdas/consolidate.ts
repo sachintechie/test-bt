4e0f91439f036c6bbceb85a34ea48e8eb132f165
Aditya Dhir <adhir@intervision.com>
2024-12-04 17:39:27 +0530

    fix

--- resources/admin_lambdas/consolidate.ts
+++ resources/admin_lambdas/consolidate.ts
@@ -7,14 +7,14 @@ export const handler = async (event: any, context: any) => {
 
     let pid = event.projectId;
     let projectName = event.projectName;
-
+    let bucketName = event.bucketName;
+    let indexName = event.indexName;
     let indexS3CreationInstance = new IndexS3Creation(projectName, pid)
-    const data = await indexS3CreationInstance.validateS3BucketName('buketname');
+    const data = await indexS3CreationInstance.validateS3BucketName(bucketName);
     // create s3 bucket
-    let bucketName = 'bucketname';
     let bucketCreationResponse = await indexS3CreationInstance.createS3Bucket(bucketName);
     console.log(bucketCreationResponse);
-    let indexCreationResponse = await indexS3CreationInstance.createOpenSearchIndex("index-name");
+    let indexCreationResponse = await indexS3CreationInstance.createOpenSearchIndex(indexName);
     console.log(indexCreationResponse);
     const kbCreationResponse = await indexS3CreationInstance.createKnowledgeBase(projectName);
     console.log(kbCreationResponse);
