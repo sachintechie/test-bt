2ff98785b2f0efc91dc712bd808e750559ca5879
Sachin Jain <sjain@intervision.com>
2024-12-17 19:14:19 +0530

    update openserach host

--- resources/admin_lambdas/updateProjectStatus.ts
+++ resources/admin_lambdas/updateProjectStatus.ts
@@ -55,23 +55,7 @@ async function updateProjectStatus(tenant: tenant, projectId: string, files: any
         const refs = await updateReferenceStatusByAdmin(files);
 
         
-  if(project != null && project.data  && project.data.knowledgebaseid == null){
-    let sanitizedName: string = project.data.name
-    .replace(/[^a-z0-9-]/g, '')   // Remove invalid characters
-    .replace(/^-+/, '')           // Remove leading hyphens
-    .replace(/^[^a-z0-9]/, 'a');  // Ensure it starts with a lowercase letter or alphanumeric
 
-  let randomString: string = await generateRandomString(6); // Generate a 6-character random string
-  let finalName: string = `${sanitizedName}-${randomString}`;
-  const kbResponse = await lambdaCallForCreateKB( project.data.id,finalName);
-  if (project != null && kbResponse && kbResponse.data != null) {
-    
-
-   const updateProject = await updateProjectKbAndIndex(project.data.id, kbResponse.data.Kb_Id ?? "",
-     kbResponse?.data.Index_Name ?? "",kbResponse?.data.Collection_Name ?? "");
-    console.log("updateProjectKB", updateProject);
-  }
-}
 
        // await addStage1Lambda(tenant.adminuserid ?? "", project.data.id,project.data.s3bucketname?? "",project.data.name);
         await addAllStageLambda(
