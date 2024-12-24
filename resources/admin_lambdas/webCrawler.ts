ab79d65b2f62d823d27802befeb1df11a788bde2
Aditya Dhir <adhir@intervision.com>
2024-12-24 15:26:46 +0530

    fix

--- resources/admin_lambdas/webCrawler.ts
+++ resources/admin_lambdas/webCrawler.ts
@@ -12,7 +12,7 @@ interface Context {
   // Define the context type if needed
 }
 
-export const lambdaHandler = async (event: any, context: Context) => {
+export const handler = async (event: any, context: Context) => {
   let count = 0;
 
   try {
