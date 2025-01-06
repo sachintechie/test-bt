2e3255966cac65b0d80b57bce3b49ada599b7965
Sachin Jain <sjain@intervision.com>
2025-01-06 11:49:59 +0530

    add project id and issue resolve of genertae q/a

--- resources/admin_lambdas/webCrawler.ts
+++ resources/admin_lambdas/webCrawler.ts
@@ -93,7 +93,7 @@ export const handler = async (event: any, context: any) => {
         const file = {
           fileName: fileName,
           fileSize: s3Data?.data?.size,
-          url:currentUrl,
+          websiteUrl:currentUrl,
           refType: RefType.DOCUMENT,
           contentType: s3Data.data?.contentType,
           hash:hash.data?.dataHash
